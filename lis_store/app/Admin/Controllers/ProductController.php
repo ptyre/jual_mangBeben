66b502b08232db7310d2de7ae12794b6210520eb
ptyre <ptyreasawau1@Gmail.com>
2018-10-24 14:40:19 +0700

    admin
    create crud admin controller

---
+++ lis_store/app/Admin/Controllers/ProductController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Product;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ProductController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product);
+
+        $grid->id('Id');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+        $grid->imagePath('ImagePath');
+        $grid->title('Title');
+        $grid->description('Description');
+        $grid->price('Price');
+        $grid->typeProduct('TypeProduct');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->id('Id');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+        $show->imagePath('ImagePath');
+        $show->title('Title');
+        $show->description('Description');
+        $show->price('Price');
+        $show->typeProduct('TypeProduct');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product);
+
+        $form->text('imagePath', 'ImagePath');
+        $form->text('title', 'Title');
+        $form->text('description', 'Description');
+        $form->number('price', 'Price');
+        $form->text('typeProduct', 'TypeProduct');
+
+        return $form;
+    }
+}
