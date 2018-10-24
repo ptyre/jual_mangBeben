edcd3e76653e93b4a0aa836c7981cc5711b1b4cb
ptyre <ptyreasawau1@Gmail.com>
2018-10-24 10:33:33 +0700

    s
    ss

--- lis_store/resources/views/shop/index.blade.php
+++ lis_store/resources/views/shop/index.blade.php
@@ -16,7 +16,7 @@
 @if(Session::has('success'))
   <div class="row"> 
     <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
-      #charge error
+      
       <div id="charge-message" class="alert alert-success">
         {{Session::get('success')}}
       </div>
